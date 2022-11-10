fa1957069ffbb7a2c02da2c5a44ded387f60bfa0
Nicolai Gram <nicolaigram@gmail.com>
2022-11-10 10:57:58 +0100

    Fireworks and google sheet

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,32 @@ import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react'
 import { DataContext, useGoogleSheetData } from './data';
 import { Main } from './sections/Main';
+import Fireworks from '@fireworks-js/react';
 
 function App() {
   const data = useGoogleSheetData()
   return (
     <ChakraProvider>
       <DataContext.Provider value={data}>
+        <Fireworks
+          options={{
+            rocketsPoint: {
+              min: 0,
+              max: 100
+            }
+          }}
+          style={{
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            position: 'fixed',
+            zIndex: '-999'
+            // background: '#000'
+          }}
+        />
         <Main />
+
       </DataContext.Provider>
     </ChakraProvider>
   );
--- src/sections/GoogleSheetLink.tsx
+++ src/sections/GoogleSheetLink.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const GoogleSheetLink = () => {
     return (
         <Center paddingBottom={"40px"}>
-            <Box><i>Google Sheet <a href="https://docs.google.com/spreadsheets/d/1Z6Ae2SFUMKkqHmFBecQCFnxYrNcKw00rqoPGcO1IYFc/edit#gid=0">her</a></i></Box>
+            <Box><i><a href="https://docs.google.com/spreadsheets/d/1Z6Ae2SFUMKkqHmFBecQCFnxYrNcKw00rqoPGcO1IYFc/edit#gid=0">Google Sheet her</a></i></Box>
         </Center>
     )
 }
