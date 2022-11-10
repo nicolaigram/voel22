2a464a1522bc4a58ae0013b5c210c25762991dda
Nicolai Gram <nicolaigram@gmail.com>
2022-11-10 10:42:00 +0100

    Fix responsive table + more

--- src/sections/Games.tsx
+++ src/sections/Games.tsx
@@ -6,7 +6,7 @@ export const Games = () => {
     const data = useContext(DataContext)
     const [games, setGames] = useState<any>([])
     useEffect(() => {
-        setGames([data["Dyst 1 - Ansvarlige"], data["Dyst 2 - Ansvarlige"], data["Dyst 3 - Ansvarlige"], data["Dyst 4 - Ansvarlige"], data["Dyst 5 - Ansvarlige"]])
+        setGames([data["Dyst 1 - Ansvarlige"], data["Dyst 2 - Ansvarlige"], data["Dyst 3 - Ansvarlige"], data["Dyst 4 - Ansvarlige"], data["Dyst 5 - Ansvarlige"], data["Dyst 6 - Ansvarlige"]])
     },[data])
 
     return <Center flexDirection={"column"} paddingBottom={"100px"}>
@@ -21,7 +21,7 @@ const renderGame = (game: any) => {
     if (!game) return
     const gameName = game[0]
     const gameMembers = game.slice(1, game.length)
-    return <Box>
+    return <Box paddingBottom={"20px"}>
         <Heading size={"md"}>{gameName}</Heading>
         {gameMembers.map((name: any) => (
             <p>{name}</p>
--- src/sections/Standings.tsx
+++ src/sections/Standings.tsx
@@ -1,6 +1,7 @@
-import { Box, Center, Heading, Table, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { Box, Center, Heading, Table, Td, Text, Th, Thead, Tr } from '@chakra-ui/react'
 import React, { useContext, useState, useEffect } from 'react'
 import { DataContext } from '../data'
+import { useMediaQuery } from 'react-responsive'
 
 export const Standings = () => {
     const data = useContext(DataContext)
@@ -10,34 +11,77 @@ export const Standings = () => {
         setTeams([data["Hold A"], data["Hold B"], data["Hold C"]].filter(e => !!e))
         setStandings([data["Dyst 1 - Score"], data["Dyst 2 - Score"], data["Dyst 3 - Score"], data["Dyst 4 - Score"], data["Dyst 5 - Score"]].filter(e => !!e))
     }, [data])
+    const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 })
 
     return <Center flexDirection={"column"} paddingBottom={"100px"}>
         <Box paddingBottom={"40px"}>
             <Heading>Stilling</Heading>
         </Box>
         <Box>
-            <Table variant={"simple"}>
-                <Thead>
-                    <Th>Disciplin</Th>
-                    {teams.map((team: any) => {
-                        const teamName = team[0]
+            {isDesktopOrLaptop ? (
+                <Table variant={"simple"} size={"sm"}>
+                    <Thead>
+                        <Th>Disciplin</Th>
+                        {teams.map((team: any) => {
+                            const teamName = team[0]
+                            return (
+                                <Th>{teamName}</Th>
+                            )
+                        })}
+                    </Thead>
+                    {standings.map((row: any) => {
+                        const gameName = row[0]
+                        const gameStandings = row.slice(1, row.length)
                         return (
-                            <Th>{teamName}</Th>
+                            <Tr>
+                                <Td>{gameName}</Td>
+                                {gameStandings.map((score: any) => (<Td>{score}</Td>))}
+                            </Tr>
                         )
                     })}
-                </Thead>
-                {standings.map((row: any) => {
-                    console.log(row)
-                    const gameName = row[0]
-                    const gameStandings = row.slice(1, row.length)
-                    return (
-                        <Tr>
-                            <Td>{gameName}</Td>
-                            {gameStandings.map((score: any) => (<Td>{score}</Td>))}
-                        </Tr>
-                    )
-                })}
-            </Table>
+                    <Tr>
+                        <Td>Total</Td>
+                        {teams.map((team: any, teamIndex: number) => {
+                            let totalScore = 0
+                            standings.forEach((row: any) => {
+                                const score = Number(row[teamIndex + 1])
+                                if (!Number.isNaN(score)) totalScore += score
+                            })
+                            return (
+                                <Td>{totalScore}</Td>
+                            )
+                        })}
+                    </Tr>
+                </Table>
+            ) : (
+                <Box>
+                    {standings.map((row: any) => {
+                        console.log(row)
+                        const gameName = row[0]
+                        const gameStandings = row.slice(1, row.length)
+                        return (
+                            <Box paddingBottom={"30px"}>
+                                <Heading size="sm" paddingBottom={"10px"}>{gameName}</Heading>
+                                {gameStandings.map((score: any, index: number) => (<Text>{teams[index][0]}: {score}</Text>))}
+                            </Box>
+                        )
+                    })}
+                    <Box>
+                        <Heading size="sm" paddingBottom={"10px"}>Total</Heading>
+                        {teams.map((team: any, teamIndex: number) => {
+                            const teamName = team[0]
+                            let totalScore = 0
+                            standings.forEach((row: any) => {
+                                const score = Number(row[teamIndex + 1])
+                                if (!Number.isNaN(score)) totalScore += score
+                            })
+                            return (
+                                <Box><Text>{teamName}: {totalScore}</Text></Box>
+                            )
+                        })}
+                    </Box>
+                </Box>
+            )}
         </Box>
     </Center>
 }
